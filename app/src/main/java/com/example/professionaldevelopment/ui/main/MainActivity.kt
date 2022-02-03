f8c321ee563a4a30c6f66c30011c5eaa53d0b0e5
robertgasp <silent.jaguar@gmail.com>
2022-02-03 19:00:08 +0300

    Lesson 6 Glide Coil Picasso Room

--- app/src/main/java/com/example/professionaldevelopment/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/professionaldevelopment/ui/main/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainScreenFragment())
